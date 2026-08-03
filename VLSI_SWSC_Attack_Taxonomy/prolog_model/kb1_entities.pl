e61367df18a4f2b820f7f7f2f6072b6c89525034
fhashmat <fhashmat@purdue.edu>
2026-08-03 15:28:59 -0400

    feat(taxonomy):add VLSI design stage entities

---
+++ VLSI_SWSC_Attack_Taxonomy/prolog_model/kb1_entities.pl
@@ -0,0 +1,18 @@
+/* KB1: Entity facts for the VLSI-SWSC attack taxonomy */
+
+%VLSI_Design_Stages
+
+stage(design_specifications). %stage0
+stage(high_level_synthesis).  %stage1
+stage(logic_synthesis). %stage2
+stage(post_synthesis_verification). %stage3
+stage(design_for_testability). %stage4
+stage(placement_and_routing). %stage5
+stage(physical_verification_and_signoff). %stage6
+stage(post_silicon_validation). %stage7
+stage(manufacturing_testing). %stage8
+
+
+
+
+
