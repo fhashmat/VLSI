a3408a35bcb0a7a3a0633d1891b00730efed5de3
fhashmat <fhashmat@purdue.edu>
2026-08-16 12:44:31 -0400

    model(taxonomy): add generated ASW Prolog facts

--- VLSI_SWSC_Attack_Taxonomy/prolog_model/load.pl
+++ VLSI_SWSC_Attack_Taxonomy/prolog_model/load.pl
@@ -1,3 +1,16 @@
+:- multifile swsc_vector/1.
+:- multifile mechanism/1.
+:- multifile enables/2.
+:- multifile possible_at/2.
+:- multifile causes/3.
+
+:- discontiguous swsc_vector/1.
+:- discontiguous mechanism/1.
+:- discontiguous enables/2.
+:- discontiguous possible_at/2.
+:- discontiguous causes/3.
+
 :- ensure_loaded(kb1_entities).
 :- ensure_loaded(kb2_relationships).
+:- ensure_loaded(kb_asw_generated).
 :- ensure_loaded(kb3_rules).
