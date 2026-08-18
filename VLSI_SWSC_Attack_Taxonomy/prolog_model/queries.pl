7b17d4f84b15c377ba25faf7d179d039ee786708
fhashmat <fhashmat@purdue.edu>
2026-08-18 14:23:50 -0400

    model(taxonomy): add demo queries

--- VLSI_SWSC_Attack_Taxonomy/prolog_model/queries.pl
+++ VLSI_SWSC_Attack_Taxonomy/prolog_model/queries.pl
@@ -1,25 +1,61 @@
-/* Example queries for the VLSI-SWSC attack taxonomy */
+% Helper queries for demonstrating the Prolog attack-path model.
 
-/*
-Query all complete attack paths:
+count_final_paths(N) :-
+    setof(path(A_sw, A_vlsi, M_sw, M_vlsi, S, O),
+          attack_path(A_sw, A_vlsi, M_sw, M_vlsi, S, O),
+          Paths),
+    length(Paths, N).
 
-?- attack_path(A_sw, M, S, O).
-*/
+count_candidate_paths(N) :-
+    setof(path(A_sw, A_vlsi, M_sw, M_vlsi, S, O),
+          candidate_attack_path(A_sw, A_vlsi, M_sw, M_vlsi, S, O),
+          Paths),
+    length(Paths, N).
 
-/*
-Query all stages affected by a specific SWSC vector:
+count_outcome(Outcome, N) :-
+    setof(path(A_sw, A_vlsi, M_sw, M_vlsi, S),
+          attack_path(A_sw, A_vlsi, M_sw, M_vlsi, S, Outcome),
+          Paths),
+    length(Paths, N).
 
-?- attack_path(distribute_backdoored_eda_tool, M, S, O).
-*/
+count_stage(Stage, N) :-
+    setof(path(A_sw, A_vlsi, M_sw, M_vlsi, O),
+          attack_path(A_sw, A_vlsi, M_sw, M_vlsi, Stage, O),
+          Paths),
+    length(Paths, N).
 
-/*
-Query all paths that lead to confidentiality outcomes:
+show_outcome_breakdown :-
+    count_outcome(confidentiality, C),
+    count_outcome(integrity, I),
+    count_outcome(availability, A),
+    format('confidentiality-~w~n', [C]),
+    format('integrity-~w~n', [I]),
+    format('availability-~w~n', [A]).
 
-?- attack_path(A_sw, M, S, confidentiality).
-*/
+show_stage_breakdown :-
+    setof(S,
+          A_sw^A_vlsi^M_sw^M_vlsi^O^attack_path(A_sw, A_vlsi, M_sw, M_vlsi, S, O),
+          Stages),
+    forall(member(S, Stages),
+           (count_stage(S, N),
+            format('~w-~w~n', [S, N]))).
 
-/*
-Query all paths possible at logic synthesis:
+show_example_path :-
+    once(attack_path(typosquatting, A_vlsi, M_sw, M_vlsi, S, O)),
+    format('typosquatting -> ~w -> ~w -> ~w -> ~w -> ~w~n',
+           [M_sw, M_vlsi, A_vlsi, S, O]).
 
-?- attack_path(A_sw, M, logic_synthesis, O).
-*/
+run_summary :-
+    count_final_paths(Final),
+    count_candidate_paths(Candidate),
+    format('final_attack_paths-~w~n', [Final]),
+    format('candidate_review_paths-~w~n', [Candidate]),
+    nl,
+    writeln('outcome_breakdown:'),
+    show_outcome_breakdown,
+    nl,
+    writeln('stage_breakdown:'),
+    show_stage_breakdown,
+    nl,
+    writeln('example_path:'),
+    show_example_path.
