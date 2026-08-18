da8d4f7bc571815c61190a0f202374126f73ab45
fhashmat <fhashmat@purdue.edu>
2026-08-18 13:21:44 -0400

    docs(model): comment Prolog loader

--- VLSI_SWSC_Attack_Taxonomy/prolog_model/load.pl
+++ VLSI_SWSC_Attack_Taxonomy/prolog_model/load.pl
@@ -1,4 +1,15 @@
-:- multifile swsc_vector/1.
+
+/* load.pl is the entry point of the prolog model. It loads all the entity files, generated facts files etc.
+:- multifile swsc_vector/1.  this tells prolog that swsc vector facts may be identified in more than 1 file. 
+ This tells Prolog that outcome/1 facts may be defined in more than one file.
+ Example outcomes: confidentiality, integrity, availability.
+:- multifile outcome/1.
+% This tells Prolog that enables/2 facts may be defined in more than one file.
+% This is used for ASW -> Msw.
+% Example: enables(typosquatting, attacker_controlled_lookalike_software_artifact).
+:- multifile enables/2.
+ */
+:- multifile swsc_vector/1. 
 :- multifile vlsi_attack_vector/1.
 :- multifile mechanism/1.
 :- multifile stage/1.
@@ -9,7 +20,10 @@
 :- multifile mechanism_enables_attack/2.
 :- multifile possible_at/2.
 :- multifile causes/3.
-
+/* 
+% This allows swsc_vector/1 facts to appear in different places/files without Prolog warning.
+:- discontiguous swsc_vector/1.
+ */
 :- discontiguous swsc_vector/1.
 :- discontiguous vlsi_attack_vector/1.
 :- discontiguous mechanism/1.
@@ -21,10 +35,14 @@
 :- discontiguous mechanism_enables_attack/2.
 :- discontiguous possible_at/2.
 :- discontiguous causes/3.
-
+/* 
+Load the basic entity definitions.
+ */
 :- ensure_loaded(kb1_entities).
 :- ensure_loaded(kb2_relationships).
 :- ensure_loaded(kb_asw_generated).
 :- ensure_loaded(kb_avlsi_generated).
 :- ensure_loaded(kb_bridge_generated).
 :- ensure_loaded(kb3_rules).
+
+
