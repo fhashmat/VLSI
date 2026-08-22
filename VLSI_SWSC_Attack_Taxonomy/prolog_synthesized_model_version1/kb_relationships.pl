055784928ccd12baf55c8febc7bdaa9479d80f32
fhashmat <fhashmat@purdue.edu>
2026-08-21 20:48:27 -0400

    Add synthesized model relationship and rule placeholders

---
+++ VLSI_SWSC_Attack_Taxonomy/prolog_synthesized_model_version1/kb_relationships.pl
@@ -0,0 +1,4 @@
+% ============================================================
+% Relationships for Synthesized Prolog Model Version 1
+% To be filled after manual review.
+% ============================================================
---
+++ VLSI_SWSC_Attack_Taxonomy/prolog_synthesized_model_version1/kb_rules.pl
@@ -0,0 +1,4 @@
+% ============================================================
+% Rules for Synthesized Prolog Model Version 1
+% To be filled after relationships are reviewed.
+% ============================================================
