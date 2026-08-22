ab035c1df97ba39a130165910b9e953223db872e
fhashmat <fhashmat@purdue.edu>
2026-08-21 20:44:00 -0400

    Add synthesized model load file

---
+++ VLSI_SWSC_Attack_Taxonomy/prolog_synthesized_model_version1/load.pl
@@ -0,0 +1,19 @@
+% ============================================================
+% Load file for Synthesized Prolog Model Version 1
+% ============================================================
+
+:- discontiguous asw/1.
+:- discontiguous msw/1.
+:- discontiguous avlsi/1.
+:- discontiguous stage/1.
+:- discontiguous outcome/1.
+
+:- discontiguous enables/2.
+:- discontiguous realizes_in_vlsi/2.
+:- discontiguous possible_at/2.
+:- discontiguous causes/3.
+
+:- ensure_loaded(kb_entities).
+:- ensure_loaded(kb_relationships).
+:- ensure_loaded(kb_rules).
+:- ensure_loaded(queries).
